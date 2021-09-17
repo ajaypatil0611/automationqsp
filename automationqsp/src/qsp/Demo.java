7f8502ca15b46430763e6792cbb617c1f77e32c5
ABC <ABC@DESKTOP-PTARR7G>
2021-09-17 09:19:49 +0530

    hii my new commit

--- automationqsp/src/qsp/Demo.java
+++ automationqsp/src/qsp/Demo.java
@@ -3,7 +3,7 @@ package qsp;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Happy Ganesh Chaturthi");
+		//System.out.println("Happy Ganesh Chaturthi");
                 System.out.println("Happy Ganesh Chaturthi to every one");
 	}
 
